c22fbb4f41a863304369fbf026f84bc0fef104f8
john <john.escuyos@98labs.com>
2016-09-21 09:30:58 +0800

    updated README

--- package.js
+++ package.js
@@ -4,7 +4,7 @@ Package.describe({
   // Brief, one-line summary of the package.
   summary: 'Simple server side data encryption using AES and Collection Hook packages',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/jeescu/meteor-server-encryption.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
